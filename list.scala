a7e9793ddb38e0708272b0d824a3138e406458fd
Farhad Islam <farhad.islam@outlook.com>
2017-11-21 22:49:09 +0000

    list.scala updated : more ways to using list added

--- list.scala
+++ list.scala
@@ -1,4 +1,13 @@
 val days1 = List("Mon", "Tues", "Wed")
 val days2 = List("Thurs", "Fri", "Sat")
-val week = days1 ++ days2
-val week = days2 ::: days2
+val weekDays1 = days1 ++ days2
+val weekDays2 = days2 ::: days2
+val weekDays3 = List(days1, days2).flatten
+val firstElement = weekDays1.head // returns first element of a List
+val lastElement =  weekDays1.tail
+val sizeOfList = weekDays1.size
+
+val isMonDayIn = weekDays1.contain("Mon")
+
+//for loop on a List
+for (x <- weekDays1) println(x)
